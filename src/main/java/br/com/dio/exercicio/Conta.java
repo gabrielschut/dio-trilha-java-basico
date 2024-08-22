c1109009ad606eda31c93e1da8c5fecb83779516
Gabriel Schütt de Almeida <gabrielschuttdealmeida@gmail.com>
2024-08-22 19:31:10 -0300

    feat: resolving the "challenge"

---
+++ src/main/java/br/com/dio/exercicio/Conta.java
@@ -0,0 +1,50 @@
+package br.com.dio.exercicio;
+
+public class Conta {
+
+    int numeroDaConta;
+    String agencia;
+    String nomeCliente;
+    double saldo;
+
+    public Conta(){}
+
+    public Conta(int numeroDaConta, String agencia, String nomeCliente, double saldo) {
+        this.numeroDaConta = numeroDaConta;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    public int getNumeroDaConta() {
+        return numeroDaConta;
+    }
+
+    public void setNumeroDaConta(int numeroDaConta) {
+        this.numeroDaConta = numeroDaConta;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
---
+++ src/main/java/br/com/dio/exercicio/ContaTerminal.java
@@ -0,0 +1,31 @@
+package br.com.dio.exercicio;
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public static Conta pegaInputUsario(){
+        String porFavor = "Por favor, digite o ";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(porFavor + "número da agência !");
+        String agencia = scanner.nextLine();
+
+        System.out.println(porFavor + "número da conta !");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println(porFavor + "nome do Cliente !");
+        String nomeCliente = scanner.next();
+
+        System.out.println(porFavor + "saldo da conta !");
+        double saldo = scanner.nextDouble();
+        return new Conta(numeroConta, agencia, nomeCliente, saldo);
+    }
+
+    public static void mensagemDeNovaConta(Conta conta){
+        String mensagem = String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e saldo %.2f já esta disponível para saque.",
+                        conta.getNomeCliente(), conta.getAgencia(), conta.getNumeroDaConta(), conta.getSaldo());
+        System.out.println("\n" + mensagem);
+    }
+
+}
